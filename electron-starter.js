ea7fd5c8cbb623aaba957f04f4a461c5c9a87fa7
sillyy <965768042@qq.com>
2020-01-08 00:42:50 +0800

    fix: allow render process use node and disable devtool warnings

--- electron-starter.js
+++ electron-starter.js
@@ -9,7 +9,14 @@ let mainWindow;
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // allow render process use node
+      nodeIntegration: true
+    }
+  });
 
   // and load the index.html of the app.
   const startUrl =
